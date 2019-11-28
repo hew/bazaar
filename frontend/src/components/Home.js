7ec40621a1a1b8e34a6ce62dfd8ff3f3d5d98e59
hew <mathujones@gmail.com>
2019-11-27 20:39:05 -0800

    feat: styles home a bit

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,18 +9,16 @@ import { Auth } from 'aws-amplify';
 export default () => {
   const hookResult = useExample();
 
-  console.log(hookResult);
-
   return match(
     hookResult,
     ({ loading }) => <Text variant="h3">Loading...</Text>,
     ({ data }) => {
       return data ? (
-        <>
-          <Text variant="h3">id: {data.id}</Text>
-          <Text variant="h2">title: {data.title}</Text>
-          <Text variant="h4">content: {data.content}</Text>
-        </>
+        <Box justify="center" px={4}>
+          <Text variant="h2">id: <Text color="text">{data.id}</Text></Text>
+          <Text variant="h2">title: <Text color="text">{data.title}</Text></Text>
+          <Text variant="h2">content: <Text color="text">{data.content}</Text></Text>
+        </Box>
       ) : (
         <Text variant="h3">Need to add some data to the DB</Text>
       );
