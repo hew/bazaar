dd9d443427506020aa68b143f22fa5582e845df7
Matt <mathujones@gmail.com>
2019-11-28 14:30:16 -0800

    Update index.js

--- frontend/src/theme-ui/index.js
+++ frontend/src/theme-ui/index.js
@@ -1,3 +1,6 @@
+// TODO: I was unable to get theme-ui extracted from the larger monorepo that it exists in, so I just plopped the files
+// in this directory for now. My efforts are over at: https://github.com/hew/theme-ui-native/
+
 export { jsx } from './jsx';
 export { ThemeProvider } from './provider';
 export { Context, useThemeUI } from './context';
